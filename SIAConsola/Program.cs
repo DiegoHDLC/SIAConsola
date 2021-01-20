7a6573db27cfeeaa2a6010f67e18580dbc5e8a7d
Diego Ignacio Herrera De La Calle <dherreradelacalle@gmail.com>
2021-01-20 19:04:19 -0300

    Agregar archivos de proyecto.

---
+++ SIAConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIAConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
